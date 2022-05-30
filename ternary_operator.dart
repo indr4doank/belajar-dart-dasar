1f9e24d23e13874188be2f379406f278ce05474a
indr4doank <54131000+indr4doank@users.noreply.github.com>
2022-05-30 15:14:29 +0700

    ternary operator di dart

---
+++ ternary_operator.dart
@@ -0,0 +1,12 @@
+void main(){
+  var nilai = 45;
+  String ucapan = nilai >= 65 ? 'selamat anada lulus' : "silahkan coba lagi";
+
+  //if(nilai >= 75){
+    //ucapan = 'selamat anda lulus';
+  //}  else{
+   // ucapan = 'silahkan coba lagi';
+  //}
+
+  print(ucapan);
+}
