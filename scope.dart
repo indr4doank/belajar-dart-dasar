3debf7c4692f82fe725b3301db057ca3820a9412
indr4doank <54131000+indr4doank@users.noreply.github.com>
2022-05-30 15:09:09 +0700

    scope di dart

---
+++ scope.dart
@@ -0,0 +1,14 @@
+void main(){
+  var name = 'indra';
+  
+  void sayHello(){
+    var hello = 'hello $name';
+    print(hello); 
+  }
+  sayHello();
+  //print(hello); error
+}
+
+void contoh(){
+  // sayHello(); error
+}
