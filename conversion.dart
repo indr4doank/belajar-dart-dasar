c0b61b1f3db4b808e5e84e505fcac87b92814e3d
indr4doank <54131000+indr4doank@users.noreply.github.com>
2022-05-30 10:40:53 +0700

    conversion tipe data

---
+++ conversion.dart
@@ -0,0 +1,17 @@
+void main(){
+  var inputString = '1000';
+  var inputInt = int.parse(inputString);
+  var inputdouble = double.parse(inputString);
+
+  print(inputString);
+  print(inputInt);
+  print(inputdouble);
+
+  var intToDouble = inputInt.toDouble();
+  var doubleToInt = inputdouble.toInt();
+
+  var intToString = inputInt.toString;
+  var doubleToString = inputdouble.toString();
+
+  
+}
