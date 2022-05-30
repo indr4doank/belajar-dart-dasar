5fc833757bd61123bc3f417fa0a8a60c7e8e151c
indr4doank <54131000+indr4doank@users.noreply.github.com>
2022-05-30 15:11:45 +0700

    switch case di dart

---
+++ switch_case.dart
@@ -0,0 +1,20 @@
+void main(){
+var nilai = 'F';
+
+switch(nilai){
+   case 'A':
+   print('anda lulus dengan baik');
+   break;
+   case 'B':
+   case 'C':
+   print('ada lulus');
+   break;
+   case 'D':
+   print('anda tidak lulus');
+   break;
+   default :
+   print('mungkin anda salah jurusan');
+
+}
+  
+}
