1517b2f399ccfabfc07975484b2e51f4d372cf98
indr4doank <54131000+indr4doank@users.noreply.github.com>
2022-05-30 10:50:45 +0700

    membuat function parameter di dart

---
+++ function_parameter.dart
@@ -0,0 +1,9 @@
+void sayHello(String namaDepan, String namaBelakang){
+  print('hello $namaDepan $namaBelakang');
+}
+
+  void main() {
+    sayHello('indra', 'wijaya');
+    sayHello('yunita', 'bebeb');
+  
+}
