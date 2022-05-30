8dc33bad6c114d6a4a3472081e0e4e68f2480f03
indr4doank <54131000+indr4doank@users.noreply.github.com>
2022-05-30 15:10:51 +0700

    string di dart

---
+++ String.dart
@@ -0,0 +1,10 @@
+void main(){
+  String firstname = 'indra';
+  String lastname = 'wijaya';
+    print(lastname);
+    print(firstname);
+
+var fullName = '$firstname ${lastname}';
+print(fullName);
+}
+
