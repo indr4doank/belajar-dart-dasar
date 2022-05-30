89c47fd41396d5f019a57ac45b89eaf98d5862e5
indr4doank <54131000+indr4doank@users.noreply.github.com>
2022-05-30 15:10:00 +0700

    set di dart

---
+++ set.dart
@@ -0,0 +1,25 @@
+void main(){
+  Set<int> numbers = {};
+  var strings = <String>{};
+  var doubles = <double>{};
+
+  print(numbers);
+
+  var names = <String>{
+    'indra',
+    'love',
+    'yunita',
+    'forever',
+    'muachhh',
+  };
+
+  //names.add('indra');
+  //names.add('love');
+  //names.add('yunita');
+  //names.add('forever');
+  //names.add('muachh');
+
+  print(names);
+  print(names.length);
+
+}
